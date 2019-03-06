ba45f8a1295a592d508a32e2052785e0bf703642
Jorge lopez <jorge.lopez@everyonecounts.com>
2019-03-06 14:18:57 -0800

    adding node localstorage to preserve data during execution

--- hooks/system-hooks.js
+++ hooks/system-hooks.js
@@ -1,8 +1,11 @@
 /* eslint-disable func-names,no-buffer-constructor */
 const { After, Before } = require('cucumber');
 const { setDefaultTimeout } = require('cucumber');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const fs = require('fs');
 
 After(async function (scenario) {
+  global.localStorage._deleteLocation();
   const world = this;
   if (scenario.result.status === 'failed') {
     const stream = await browser.takeScreenshot();
@@ -12,6 +15,11 @@ After(async function (scenario) {
 });
 
 Before(async () => {
+  const dir = `${__dirname}/../../../node-storage`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
   setDefaultTimeout(60000);
   browser.manage().timeouts().setScriptTimeout(60000);
   browser.manage().window().maximize();
